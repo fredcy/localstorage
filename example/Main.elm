11dba0bd51cd5e670f64eab340c6cfc26bd5c99c
Fred Yankowski <fcy@imsa.edu>
2016-05-19 19:18:31 -0500

    Works now, after hacking username/repo in elm-package.json

--- example/Main.elm
+++ example/Main.elm
@@ -1,11 +1,13 @@
 module Main exposing (..)
 
+import Basics.Extra exposing (never)
 import Html exposing (Html)
 import Html.App as Html
 import Window
-import Native.Window
+import Task
 
 
+main : Program Never
 main =
     Html.program
         { init = init
@@ -15,17 +17,33 @@ main =
         }
 
 
+type alias Model =
+    Window.Size
+
+
+init : ( Model, Cmd Msg )
 init =
-    ( { width = 0, height = 0 }, Cmd.none )
+    ( { width = 0, height = 0 }
+    , Task.perform never WindowSize Window.size
+    )
 
 
+type Msg
+    = WindowSize Window.Size
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    model ! []
+    case msg of
+        WindowSize size ->
+            size ! []
 
 
+view : Model -> Html Msg
 view model =
     Html.div [] [ Html.text (toString model) ]
 
 
+subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Window.resizes WindowSize
